fbae345e84d3f5ada38eddee86ff914588f386b7
Firman Nur Budiman <firmannur21@if.unjani.ac.id>
2024-01-11 14:51:46 +0700

    MENAMBAHKAN PHP

---
+++ PHP/ta1.php
@@ -0,0 +1,28 @@
+<?php
+require 'koneksi.php';
+
+$nama = $_POST["nama"];
+$nim = $_POST["nim"];
+$prodi = $_POST["prodi"];
+$email = $_POST["email"];
+$no_hp = $_POST["no_hp"];
+$ipk = $_POST["ipk"];
+$judul = $_POST["judul"];
+$pilih_pembimbing = $_POST["pembimbing"];
+
+$query_sql = "INSERT INTO ta1 (nama, nim, prodi, email, no_hp, ipk, judul, pembimbing ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+if($stmt = mysqli_prepare($conn, $query_sql)){
+    mysqli_stmt_bind_param($stmt, "ssssssss", $nama, $nim, $prodi, $email, $no_hp, $ipk, $judul, $pilih_pembimbing);
+    
+    if(mysqli_stmt_execute($stmt)){
+        header("Location: TA1.html");
+    } else {
+        echo "Pendaftaran GAGAL :" . mysqli_error($conn);
+    }
+} else {
+    echo "Pendaftaran GAGAL :" . mysqli_error($conn);
+}
+
+?>
